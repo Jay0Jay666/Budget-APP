f7a6d7f8d0af3f5d25cefc41541aecb55bdea722
Jay0Jay666 <121548115+Jay0Jay666@users.noreply.github.com>
2025-01-18 22:59:22 +0200

    added profile

---
+++ Budget APP/Models/DataBase/profile.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Budget_APP.Models.DataBase
+{
+    [Table("profile")]
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }= string.Empty;
+        public Profile(int id, string name, string surname, string email, string password, string username)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+            Username = username;
+        }
+    }
+}
